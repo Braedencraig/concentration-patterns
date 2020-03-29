c07479879778d5dca0ac53b864991bbada55bac1
Braeden <braeden.craig@gmail.com>
2020-03-29 11:24:12 -0400

    adjust play button

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,56 +1,62 @@
-import React from 'react'
+import React from 'react';
 import logo from './logo.png';
 
 class Index extends React.Component {
   constructor() {
-      super()
-      this.state = {
-          active: false
-      }
-	}
+    super();
+    this.state = {
+      active: false,
+    };
+  }
 
-	componentWillUpdate() {
-		if(this.state.active === false) {
-			document.getElementById('player').play()
-		} else {
-			document.getElementById('player').pause()
-		}
-	}
-	
+  componentWillUpdate() {
+    if (this.state.active === false) {
+      document.getElementById('player').play();
+    } else {
+      document.getElementById('player').pause();
+    }
+  }
+
   render() {
-		const string = `//`
+    const string = `//`;
 
-		const handleClick = (e, state) => {
-			this.setState({active: !this.state.active})
-		}
+    const handleClick = (e, state) => {
+      this.setState({ active: !this.state.active });
+    };
 
-    return(
+    return (
       <>
-      	<div className='image'>
-        	<img src={logo} className='App-logo' alt='logo' />
-					<div className="video" onClick={(e) => handleClick(e)} >
-          	<div id="play-btn" className={this.state.active === false ? "triangle-mode" : "rectangle-mode"}></div>
-        	</div>
+        <div className='image'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <div className='video' onClick={e => handleClick(e)}>
+            <div
+              id='play-btn'
+              className={
+                this.state.active === false ? 'triangle-mode' : 'rectangle-mode'
+              }
+            ></div>
+          </div>
         </div>
         <div className='text'>
-        <h3>{`${string}///PERPETUAL STREAM///${string}`}</h3>
-				<h3><a href ="https://hiddenharmony.bigcartel.com/product/c-r-gillespie-concentration-patterns-2lp-hh01">PRE ORDER</a></h3>
+          <h3>{`${string}///PERPETUAL STREAM///${string}`}</h3>
+          <h3>
+            <a href='https://hiddenharmony.bigcartel.com/product/c-r-gillespie-concentration-patterns-2lp-hh01'>
+              PRE ORDER
+            </a>
+          </h3>
 
           <p>May 1st</p>
           <p>Hidden Harmony Recordings</p>
           <p>(HH01)</p>
         </div>
         <audio
-        	src='http://edge.mixlr.com/channel/onuip'
+          src='http://edge.mixlr.com/channel/evezj'
           type='audio/mpeg'
-					
-				
-          id="player"
+          id='player'
         ></audio>
       </>
-    )
+    );
   }
 }
-
 
 export default Index;
