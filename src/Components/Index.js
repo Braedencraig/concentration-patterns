dba6be240caa7fbe53cb6137c57191211e02fd92
Braeden <braeden.craig@gmail.com>
2020-03-29 11:50:36 -0400

    fixed album text spacing

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -45,7 +45,7 @@ class Index extends React.Component {
             </a>
           </h3>
 
-          <p>May 1st</p>
+          <p>Out May 1st on 2xLP</p>
           <p>Hidden Harmony Recordings</p>
           <p>(HH01)</p>
         </div>
